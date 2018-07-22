36c20091d3b5749f3ff9eb26a9547c5a37c0516f
github username <github email address>
2018-07-22 13:39:05 -0400

    final prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require 'pry'
+
+def prime?(input)
+  if input < 0
+    input = -1 * input
+  end
+
+  if input == 1 || input == 0
+    return false
+  end
+
+  range = (2..99).to_a
+  new_range = range.select do |i|
+    i != input
+  end
+    # binding.pry
+  for int in new_range
+    if input % int == 0
+      return false
+    end
+  end
+  return true
+end
